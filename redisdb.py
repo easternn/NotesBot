6aca2d48f6662496fb3d3ec2e0e0e1102b6fc68f
Timur Ratnikov <timur2640rt@gmail.com>
2022-12-30 21:19:04 +0600

    Add creating notes & show all notes

--- redisdb.py
+++ redisdb.py
@@ -1,11 +1,83 @@
 from redis import asyncio as aioredis
 
-r = aioredis.from_url('redis://localhost')
+r = aioredis.from_url('redis://localhost', decode_responses=True)
+
+
+async def debug(user_Id):
+    name = await r.get("user:{}".format(user_Id))
+    processed_q = await r.get("user:{}:q".format(user_Id))
+    qrs = await r.lrange("user:{}:qrs".format(user_Id), 0, -1)
+    status = await r.get("user:{}:status".format(user_Id))
+    print("##########")
+    print("Used-id: {}".format(user_Id))
+    print("Name: {}".format(name))
+    print("processed_q: {}".format(processed_q))
+    print(qrs)
+    print("status: {}".format(status))
+    print("##########")
+    print()
+
+
+async def get_status(user_Id):
+    return await r.get("user:{}:status".format(user_Id))
+
+
+async def validate_answer(answer):
+    return True
+
+
+async def validate_question(question):
+    return True
 
 
 async def add_user(user_Id, first_name, last_name):
+    print("START add_user")
+    await debug(user_Id)
     user = await r.get("user:{}".format(user_Id))
     if user is None:
         await r.set("user:{}".format(user_Id), "{} {}".format(first_name, last_name))
-    else:
-        print(user)
+    await r.set("user:{}:status".format(user_Id), "Default")
+    print("END add_user")
+    await debug(user_Id)
+
+
+async def plan_question(user_Id):
+    print("START plan_question")
+    await debug(user_Id)
+    await r.set("user:{}:status".format(user_Id), "Awaiting-question")
+    print("END plan_question")
+    await debug(user_Id)
+
+
+async def add_question(user_Id, question):
+    print("START add_question")
+    await debug(user_Id)
+    status = await r.get("user:{}:status".format(user_Id))
+    if status != "Awaiting-question":
+        return
+    if not await validate_question(question):
+        return
+    await r.set("user:{}:q".format(user_Id), question)
+    await r.set("user:{}:status".format(user_Id), "Awaiting-response")
+    print("END add_question")
+    await debug(user_Id)
+
+
+async def add_answer(user_Id, answer):
+    print("START add_answer")
+    await debug(user_Id)
+    status = await r.get("user:{}:status".format(user_Id))
+    if status != "Awaiting-response":
+        return
+    if not await validate_answer(answer):
+        return
+    question = await r.get("user:{}:q".format(user_Id))
+    await r.rpush("user:{}:qrs".format(user_Id), "{}{}".format(question, answer))
+    await r.set("user:{}:status".format(user_Id), "Default")
+    print("END add_answer")
+    await debug(user_Id)
+
+
+async def get_notes(user_Id):
+    qrs = await r.lrange("user:{}:qrs".format(user_Id), 0, -1)
+    return qrs
--- worker.py
+++ worker.py
@@ -3,11 +3,30 @@ import telegram
 import redisdb
 
 
+async def show_all_notes(tg_client: telegram.Bot, user_id, notes):
+    for note in notes:
+        question, answer = note.split('?')
+        question += '?'
+        await tg_client.send_message(chat_id=user_id, text='{}\n\n{}'.format(question, answer))
+
+
 async def handle_update(tg_client: telegram.Bot, upd: telegram.Update):
     user_id = upd.effective_chat.id
     text = upd.message.text
     if text == '/start':
         await redisdb.add_user(user_id, upd.effective_chat.first_name, upd.effective_chat.last_name)
+    elif text == '/create':
+        await redisdb.plan_question(user_id)
+    elif text == '/all':
+        notes = await redisdb.get_notes(user_id)
+        await show_all_notes(tg_client, user_id, notes)
+    else:
+        status = await redisdb.get_status(user_id)
+        print(status)
+        if status == 'Awaiting-question':
+            await redisdb.add_question(user_id, text)
+        elif status == 'Awaiting-response':
+            await redisdb.add_answer(user_id, text)
 
 
 class Worker:
